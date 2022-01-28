63e3f67e0cd0890c848c430f13e66a08db21a258
Kyle Danny <codingkyle92@gmail.com>
2022-01-28 23:40:45 +0100

    extending interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,78 @@
-// type Addfn = (a: number, b: number) => number;
-interface Addfn {
-  (a: number, b: number): number;
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universe = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
-let add: Addfn;
+type UnknownEmployee = Employee | Admin;
 
-add = (n1: number, n2: number) => {
-  return n1 + n2;
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
 }
 
-interface Named {
-  readonly name?: string;
-  outputName?: string;
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
 }
 
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
-
-class Person implements Greetable {
-  name?: string;
-  age = 30;
-
-  constructor(n?: string) {
-    if (n) {
-      this.name = n;
-    }
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
   }
 
-  greet(phrase: string) {
-    if (this.name) {
-      console.log(phrase + ' ' + this.name);
-    } else {
-      console.log('Hi!')
-    }
+  loadCargo(amount: number) {
+    console.log("Loading cargo ... " + amount);
   }
 }
 
-let user1: Greetable;
+type Vehicle = Car | Truck; 
 
-user1 = new Person();
+const v1 = new Car();
+const v2 = new Truck();
 
-user1.greet('Hey there - I am')
-console.log(user1);
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+
